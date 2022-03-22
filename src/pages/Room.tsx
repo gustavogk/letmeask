e63b4a4a8374cf598bbeaab692a1da6b12e5bc3c
Gustavo Assuncao <gust.krv@gmail.com>
2022-03-22 16:20:16 -0300

    questions list

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom'
 import logoImg from '../assets/images/logo.svg';
 
 import { Button } from '../components/Button';
+import { Question } from '../components/Question';
 import { RoomCode } from '../components/RoomCode';
 import { useAuth } from '../hooks/useAuth';
 
@@ -21,7 +22,7 @@ type FirebaseQuestions = Record<string, {
     isHighlighted: boolean;
 }>
 
-type Question = {
+type QuestionType = {
     id: string;
     author: {
         name: string;
@@ -42,7 +43,7 @@ export function Room(){
     const {user} = useAuth();
     const params = useParams<RoomParams>();
     const [newQuestion, setNewQuestion] = useState('');
-    const [questions, setQuestions] = useState<Question[]>([]);
+    const [questions, setQuestions] = useState<QuestionType[]>([]);
     const [title, setTittle] = useState('');
 
     const roomId = params.id;
@@ -142,7 +143,18 @@ export function Room(){
                     </div>
                 </form>
 
-                {JSON.stringify(questions)}
+                <div className='question-list'>
+                    {questions.map( question => {
+                        return (
+                            <Question
+                                key={question.id}
+                                content={question.content}
+                                author={question.author}
+                            />
+                        );
+                    })}
+                </div>
+               
             </main>
         </div>
     );
